b1897b8a76329662f10e030d8a78e954bdd72213
Ali Hasan <ali.hasan@northsouth.edu>
2018-10-03 23:54:02 +0600

    another prolem sloved

---
+++ dataType.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+intergerTwo=int(input())
+doubleTwo=float(input())
+stg2=input()
+
+
+# Print the sum of both integer variables on a new line.
+print (i +intergerTwo)
+
+# Print the sum of the double variables on a new line.
+print (d+ doubleTwo)
+# Concatenate and print the String variables on a new line
+print (s +stg2)
